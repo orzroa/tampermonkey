dbecdb49109127967098c37939425e6e4d3a7991
orzroa <orzroa@gmail.com>
2019-04-23 15:53:19 +0800

    Update cleanCSDN.user.js

--- cleanCSDN.user.js
+++ cleanCSDN.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         cleanCSDN
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://blog.csdn.net/*/article/details/*
@@ -10,7 +10,12 @@
 
 (function() {
     'use strict';
-    document.getElementsByClassName("container clearfix").mainBox.children[0].style.display='none'
-    document.getElementsByClassName("container clearfix").mainBox.children[1].style.width=window.innerWidth-140+"px"
-    document.getElementsByClassName("container clearfix").mainBox.children[2].style.display='none'
+    var mainBox = document.getElementById("mainBox")
+    var windowWidth = window.innerWidth
+    mainBox.style.width=windowWidth-10+"px"
+    mainBox.children[0].style.display='none'
+    mainBox.children[1].style.width=windowWidth-10+"px"
+    mainBox.children[2].style.display='none'
+    document.getElementsByClassName("pulllog-box")[0].style.display='none'
+    document.getElementsByClassName("meau-gotop-box")[0].style.display='none'
 })();
